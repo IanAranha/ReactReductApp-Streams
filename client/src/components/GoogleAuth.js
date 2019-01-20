bf5ef59d3ce7a20ea85208cb7c72baf43433687c
Ian Aranha <ianorama@gmail.com>
2019-01-19 21:23:43 -0800

    Functional login which updates state and stays updated. Achieved by adding .listen()

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -7,19 +7,33 @@ class GoogleAuth extends React.Component{
     //This section will load the entire Google Sign In library.
     //It will wait till the entire library loads and then authenticate the key.
     //After loading, it will authenticate the signer in and state is changed 
+    //The program waits till the entire library is loaded/initialized and the 
+    //sign-in is authenticated.
     componentDidMount(){
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '526453861795-m5rvehdqeadi0a1f1uetudpt49feeudn.apps.googleusercontent.com',
-                scope: 'email'
-            }).then(()=>{
-                this.auth = window.gapi.auth2.getAuthInstance()
-                this.setState({
-                    isSignedIn: this.auth.isSignedIn.get()
-                })
+                scope: 'email'}).then(()=>{
+                    //Authorization object is loaded
+                    this.auth = window.gapi.auth2.getAuthInstance()
+                    //State is set depending on successful login
+                    this.setState({
+                        isSignedIn: this.auth.isSignedIn.get()
+                    })
+                    //this function listens to state throughout....keeps log in constant.
+                    this.auth.isSignedIn.listen(this.onAuthChange)
             })
         })
     }
+
+    onAuthChange = () => {
+        this.setState({
+            isSignedIn: this.auth.isSignedIn.get()
+        })
+    }
+
+
+
     //Helper function that will read state and then place appropriate 
     //status in the header bar.
     renderAuthButton(){
